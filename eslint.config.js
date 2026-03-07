49e8eca98bad00c24f22bd9ae7520519c4af15ba
Afroz-J-Shaikh <afsk1997@gmail.com>
2026-03-08 00:55:30 +0530

    added workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,13 @@
 // eslint.config.js
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module"
-    },
-    plugins: {
-      react,
-      "react-hooks": reactHooks
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off"
     }
   }
 ];
